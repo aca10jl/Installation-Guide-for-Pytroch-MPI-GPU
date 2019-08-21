e11091eee1438f906fc7ee97011833575718e443
CB6 <aca10jl@users.noreply.github.com>
2019-08-21 15:31:40 +0100

    Update TorchCudaMPI.py

--- TorchCudaMPI.py
+++ TorchCudaMPI.py
@@ -1,19 +1,17 @@
 import torch.distributed as dist
 import torch
 
-def debug_fun():
+def main():
     torch.manual_seed(dist.get_rank())
-    # device = "cuda:{}".format(dist.get_rank())
-    device = "cuda:0".format(dist.get_rank())
-    # device = "cpu"
+    device = "cuda:{}".format(dist.get_rank())
     param = torch.FloatTensor(1,1).to(device)
 
     param.uniform_()
     print("Rank {}, before sync: {} at {}".format(dist.get_rank(),param.cpu().data.numpy(), param.device))
-    dist.broadcast(param.data,0)
+    dist.broadcast(param.data, 0)
     print("Rank {}, after sync : {} at {}".format(dist.get_rank(),param.cpu().data.numpy(), param.device))
 
 
 if __name__ == '__main__':
-    dist.init_process_group("mpi", rank=0, world_size=0)
-    debug_fun()
+    dist.init_process_group("mpi")
+    main()
